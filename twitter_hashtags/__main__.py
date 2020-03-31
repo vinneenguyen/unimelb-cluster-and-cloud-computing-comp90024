5d24a0de0a6aa3c5efff0f7064ccf03727162aab
Vinh Xuan Nguyen <vinh@spartan-login2.hpc.unimelb.edu.au>
2020-04-01 01:43:26 +1100

    create .gitignore

--- twitter_hashtags/__main__.py
+++ twitter_hashtags/__main__.py
@@ -3,11 +3,14 @@
 import sys
 from mpi4py import MPI
 
-filename = sys.argv[1] # data file
-size = MPI.COMM_WORLD.Get_size() # total number of processors
-rank = MPI.COMM_WORLD.Get_rank() # processor number
+DATAFILE = sys.argv[1] # json tweet data
+SIZE = MPI.COMM_WORLD.Get_size() # total number of processors
+RANK = MPI.COMM_WORLD.Get_rank() # processor number
+
+
 
 def main():
     print("Helloworld! I am process %d of %d.\n" % (rank, size))
+
     
 main()
