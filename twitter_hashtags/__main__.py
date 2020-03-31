ddcbe15c222aebe515fee91055f678726c1a22c2
Vinh Xuan Nguyen <vinh@spartan-login2.hpc.unimelb.edu.au>
2020-04-01 01:01:55 +1100

    update __main_.py

--- twitter_hashtags/__main__.py
+++ twitter_hashtags/__main__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from mpi4py import MPI
 import sys
+from mpi4py import MPI
 
-size = MPI.COMM_WORLD.Get_size()
-rank = MPI.COMM_WORLD.Get_rank()
+filename = sys.argv[1] # data file
+size = MPI.COMM_WORLD.Get_size() # total number of processors
+rank = MPI.COMM_WORLD.Get_rank() # processor number
 
 def main():
     print("Helloworld! I am process %d of %d.\n" % (rank, size))
