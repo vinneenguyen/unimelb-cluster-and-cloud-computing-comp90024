0341859365716fc0d5e9d885b85007ad4f86ee45
nxv1987 <nxv1987@gmail.com>
2020-04-01 04:17:24 +1100

    update utils/

--- twitter_hashtags/__main__.py
+++ twitter_hashtags/__main__.py
@@ -1,16 +1,13 @@
-# -*- coding: utf-8 -*-
-
 import sys
 from mpi4py import MPI
 
-DATAFILE = sys.argv[1] # json tweet data
-SIZE = MPI.COMM_WORLD.Get_size() # total number of processors
-RANK = MPI.COMM_WORLD.Get_rank() # processor number
-
+DATAFILE = sys.argv[1]  # json tweet data
+SIZE = MPI.COMM_WORLD.Get_size()  # total number of processors
+RANK = MPI.COMM_WORLD.Get_rank()  # processor number
 
 
 def main():
     print("Helloworld! I am process %d of %d.\n" % (rank, size))
 
-    
+
 main()
