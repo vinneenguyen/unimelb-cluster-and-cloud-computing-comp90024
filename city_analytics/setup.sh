9965678262877d76819418506537d168c2202682
Adam Mooy <houmun.mooy@gmail.com>
2020-05-03 14:13:21 +1000

    setup.sh changes

--- city_analytics/setup.sh
+++ city_analytics/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-<<<<<<< HEAD
+
 ansible-galaxy install -r roles/requirements.yml  # install roles
 chmod +x inventories/dynamic.py  # make script executable
 inventories/dynamic.py --list  # test by retrieving instance information
@@ -12,7 +12,10 @@ ansible-playbook -i inventories/hosts.yaml volumes_attach.yaml  # attach volumes
 ansible-playbook -i inventories/dynamic.py -i inventories/hosts.yaml inventory_export.yaml  # generate inventory file with IP addresses
 ansible-playbook -i hosts proxy_config.yaml  # Add proxy to etc/environment and docker environment
 ansible-playbook -i inventories/hosts_auto.ini dependencies.yaml  # install dependencies on servers
-=======
+# ansible-playbook -i inventories/hosts_auto.ini -i inventories/group_vars/webservers.yaml common.yaml
+# ansible-playbook -i inventories/hosts_auto.ini -i inventories/group_vars/webservers.yaml docker.yaml 
+ansible-playbook -i inventories/hosts_auto.ini -i inventories/group_vars/webservers.yaml couchdb_setup.yaml --skip-tags "remove_directory" # Create couchDB cluster, (CAUTION) comment skip-tags to remove couchDB data on instance
+
 # ansible-galaxy install -r roles/requirements.yml  # install roles
 # chmod +x inventories/dynamic.py  # make script executable
 # inventories/dynamic.py --list  # test by retrieving instance information
@@ -24,11 +27,9 @@ ansible-playbook -i inventories/hosts_auto.ini dependencies.yaml  # install depe
 # ansible-playbook -i inventories/hosts.yaml volumes_attach.yaml  # attach volumes to instances
 # ansible-playbook -i inventories/dynamic.py -i inventories/hosts.yaml inventory_export.yaml  # generate inventory file with IP addresses
 # ansible-playbook -i inventories/hosts_auto.ini -i inventories/group_vars/webservers.yaml proxy_config.yaml  # Add proxy to etc/environment and docker environment
-# ansible-playbook -i inventories/hosts_auto.ini -i inventories/group_vars/webservers.yaml common.yaml
-# ansible-playbook -i inventories/hosts_auto.ini -i inventories/group_vars/webservers.yaml docker.yaml 
-ansible-playbook -i inventories/hosts_auto.ini -i inventories/group_vars/webservers.yaml couchdb_setup.yaml --skip-tags "remove_directory" # Create couchDB cluster, (CAUTION) comment skip-tags to remove couchDB data on instance
+
 
 
 # Use for Debugging 
 # ansible-playbook -i inventories/hosts_auto.ini test.yaml
->>>>>>> 2220751d66836c937b59fe54e6d78c87f965ea39
+
