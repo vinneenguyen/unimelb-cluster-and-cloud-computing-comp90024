5b570f285637331a025fb056048740c63dde8164
Adam Mooy <houmun.mooy@gmail.com>
2020-05-14 15:23:04 +1000

    Isolated Stage 1

--- city_analytics/setup.sh
+++ city_analytics/setup.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-# chmod +x inventories/dynamic.py  # make script executable
-# inventories/dynamic.py --list  # test by retrieving instance information
-# ansible-playbook security_groups.yaml  # create security groups
-# ansible-playbook keypairs.yaml  # generate keypairs
-# ansible-playbook -i inventories/hosts.yaml instances.yaml  # launch compute instances
-# ansible-playbook -i inventories/hosts.yaml volumes.yaml  # create volumes
-# ansible-playbook -i inventories/hosts.yaml volumes_attach.yaml  # attach volumes to instances
-# ansible-playbook -i inventories/dynamic.py -i inventories/hosts.yaml inventory_export.yaml  # generate inventory file with IP addresses
-# ansible-playbook -i inventories/hosts_auto.ini proxy_config.yaml  # Add proxy to etc/environment and docker environment
-# ansible-playbook -i inventories/hosts_auto.ini dependencies.yaml
-# ansible-playbook -i inventories/hosts_auto.ini formatvolume.yaml --skip-tags "format" # Format attached volume, *under construction
-# ansible-playbook -i inventories/hosts_auto.ini docker.yaml
-ansible-playbook -i inventories/hosts_auto.ini applications.yaml #--skip-tags "kill_container" # Docker-compose application under 
-ansible-playbook -i inventories/hosts_auto.ini couchDB_setup.yaml  # Create couchDB cluster
+chmod +x inventories/dynamic.py  # make script executable
+inventories/dynamic.py --list  # test by retrieving instance information
+ansible-playbook security_groups.yaml  # create security groups
+ansible-playbook keypairs.yaml  # generate keypairs
+ansible-playbook -i inventories/hosts.yaml instances.yaml  # launch compute instances
+ansible-playbook -i inventories/hosts.yaml volumes.yaml  # create volumes
+ansible-playbook -i inventories/hosts.yaml volumes_attach.yaml  # attach volumes to instances
+ansible-playbook -i inventories/dynamic.py -i inventories/hosts.yaml inventory_export.yaml  # generate inventory file with IP addresses
+ansible-playbook -i inventories/hosts_auto.ini proxy_config.yaml  # Add proxy to etc/environment and docker environment
+ansible-playbook -i inventories/hosts_auto.ini dependencies.yaml
+ansible-playbook -i inventories/hosts_auto.ini formatvolume.yaml --skip-tags "format" # Format attached volume, *under construction
+ansible-playbook -i inventories/hosts_auto.ini docker.yaml
+ansible-playbook -i inventories/hosts_auto.ini copy_master.yaml
 
 
 # Use for Debugging 
-# ansible-playbook -i inventories/hosts_auto.ini test.yaml
+# ansible-playbook -i inventories/hosts.yaml test.yaml
