e117fc4b92ee85d830634eccb9711336db63451f
nxv1987 <nxv1987@gmail.com>
2020-05-26 19:19:50 +1000

    update app city_analytics/geo

--- city_analytics/geo/__main__.py
+++ city_analytics/geo/__main__.py
@@ -4,7 +4,6 @@ import configparser
 from pathlib import Path
 import pandas as pd
 import geopandas as gpd
-from flask import Flask, redirect, url_for, request, render_template
 from IPython.display import display
 
 from utils.couch import view_covid
@@ -161,7 +160,7 @@ def _do_symptoms():
     ax.get_figure().savefig(outfile, bbox_inches="tight")
 
 
-def visualize():
+def main():
     """
     Create graphs
     :return:
@@ -171,45 +170,9 @@ def visualize():
     _do_symptoms()
 
 
-def _webapp():
-
-    app = Flask(__name__, static_folder='../georesults', template_folder='../georesults')
-
-    @app.route('/')
-    def index():
-        data1 = 3333
-        data2 = 6666
-        return render_template('index.html', data1=data1, data2=data2)
-
-    @app.route('/get_map1')
-    def get_map1():
-        return render_template('covid_sa4.html')
-
-    @app.route('/get_map2')
-    def get_map2():
-        return render_template('covidsafe_sentiment_sa4.html')
-
-    @app.route('/get_map3')
-    def get_map3():
-        return render_template('chart1.html')
-
-    @app.route('/get_map4')
-    def get_map4():
-        return render_template('chart2.html')
-
-    app.run(
-      #host='0.0.0.0',
-      #port= 6666,
-      debug=True
-    )
-
-
 if __name__ == "__main__":
-    _webapp()
-    # Run periodically
-    
     cyc = 900  # seconds
     while True:
-        visualize()
+        main()
         print(f"Next execution scheduled at {datetime.now() + timedelta(seconds=cyc)}")
         time.sleep(cyc)
